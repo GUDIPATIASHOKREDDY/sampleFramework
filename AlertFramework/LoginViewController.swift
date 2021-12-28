e5d769b0c6cfe34745a064e6f838ff727244132b
Ashok <gudipatiashok205@gmail.com>
2021-12-28 19:22:21 +0530

    login vc

---
+++ AlertFramework/LoginViewController.swift
@@ -0,0 +1,33 @@
+//
+//  LoginViewController.swift
+//  AlertFramework
+//
+//  Created by GUDIPATI ASHOK REDDY on 28/12/21.
+//
+
+import UIKit
+
+public class LoginViewController: UIViewController {
+    @IBOutlet weak var password: UITextField!
+    
+    @IBOutlet weak var userName: UITextField!
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func LoginAction(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
