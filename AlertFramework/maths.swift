2ac7460cc4d96eae8940072f59667b7a9c749332
Ashok <gudipatiashok205@gmail.com>
2021-12-28 18:48:15 +0530

    Alert added

--- AlertFramework/maths.swift
+++ AlertFramework/maths.swift
@@ -6,8 +6,19 @@
 //
 
 import Foundation
+import UIKit
 
 public func somecalulation(a: Int , b: Int) -> Int  {
   let sum =  a + b
   return sum
+}
+
+public func showAlert(vc:UIViewController) {
+    let alert = UIAlertController(title: "TEST", message: "WORKING", preferredStyle: UIAlertController.Style.alert)
+
+           // add an action (button)
+    alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+
+           // show the alert
+    vc.present(alert, animated: true, completion: nil)
 }
