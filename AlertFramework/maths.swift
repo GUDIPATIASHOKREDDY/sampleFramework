9fe728521835790938b854b579fffbd209363b6a
Ashok <gudipatiashok205@gmail.com>
2021-12-28 17:00:27 +0530

    initial commit

---
+++ AlertFramework/maths.swift
@@ -0,0 +1,13 @@
+//
+//  maths.swift
+//  AlertFramework
+//
+//  Created by GUDIPATI ASHOK REDDY on 28/12/21.
+//
+
+import Foundation
+
+public func somecalulation(a: Int , b: Int) -> Int  {
+  let sum =  a + b
+  return sum
+}
